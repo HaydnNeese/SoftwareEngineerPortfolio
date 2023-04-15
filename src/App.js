27b820e29bdbf53a1aa4ddd6e87f4befc284d8fe
Haydn Neese <haydnneese@yahoo.com>
2023-04-14 22:12:01 -0500

    set up home component and body

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Header from './Header/Header';
+import Home from './Home/Home';
 
 function App() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+
+      <Home/>
+    </div>
   );
 }
 
