5c1264f7c58184669bf3c95eba8e3270c7e5bdef
Haydn Neese <haydnneese@yahoo.com>
2023-04-16 17:56:48 -0500

    changed color theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 import Header from './Header/Header';
 import Home from './Home/Home';
+import { createTheme, colors, ThemeProvider } from '@mui/material';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main:  colors.grey[900],
+        light: colors.blueGrey[400],
+        dark: colors.blueGrey[200],
+        contrastText: colors.grey[50]
+      },
+      secondary: {
+        main: colors.grey[200],
+        light: colors.grey[50],
+        dark: colors.grey[600],
+        contrastText: colors.blueGrey[800]
+      }
+    }
+  });
+
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Header/>
 
       <Home/>
-    </div>
+    </ThemeProvider>
   );
 }
 
