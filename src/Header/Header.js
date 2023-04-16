fdcb93bd8444e43b166d733d720976d1803d3a8e
Haydn Neese <haydnneese@yahoo.com>
2023-04-16 14:23:40 -0500

    delete old header code

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,19 +2,8 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className='header'>
-            <h1>Neese</h1>
-
-            <div className='link-container'>
-                {/* TODO link component */}
-                <div>Home</div>
-                {/* TODO link component */}
-                <div>About Me</div>
-                {/* TODO link component */}
-                <div>Skills</div>
-                {/* TODO link component */}
-                <div>Applications</div>
-            </div>
+        <div >
+            
         </div>
     )
 };
